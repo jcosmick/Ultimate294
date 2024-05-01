56429c951b881ab147decdf1c5a987bce326d250
jcosmick <jcosmick@gmail.com>
2024-05-01 21:57:17 +0200

    Added an utils method to manage distance

---
+++ SCP294/Utils/DistanceUtils.cs
@@ -0,0 +1,32 @@
+﻿using PluginAPI.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SCP294.Utils
+{
+    internal class DistanceUtils
+    {
+        public static bool isInRange(Vector3 pos1, Vector3 pos2, float range)
+        {
+            return Vector3.Distance(pos1, pos2) < range;
+        }
+
+        public static List<Player> getAllPlayersInRange(Vector3 center, float range)
+        {
+            List<Player> playersInRange = new List<Player>();
+            foreach (var player in Player.GetPlayers())
+            {
+                if (player.IsAlive)
+                {
+                    playersInRange.Add(player);
+                }
+            }
+            Log.Debug(playersInRange.ToArray().ToString());
+            return playersInRange;
+        }
+    }
+}
